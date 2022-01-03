f18ac47f47b5ed3110cd4a4e574d7f771fee1f9f
augcos <augcos@protonmail.com>
2022-01-03 13:55:18 +0100

    removal of order history analysis from loop

--- dashboard.py
+++ dashboard.py
@@ -31,49 +31,46 @@ path =os.path.dirname(os.path.realpath(__file__))
 path = os.path.join(path,'myOrders.csv')
 myOrders = pd.read_csv(path)
 myOrders = myOrders[::-1]
-myOrders = myOrders[(myOrders['Status'].isin(['FILLED', 'PARTIALLY_FILLED'])) & (myOrders['Pair'].str.contains('BTC')) & \
+myOrders = myOrders[(myOrders['Status'].isin(['FILLED', 'PARTIALLY_FILLED'])) & (myOrders['Pair'].str.contains('BTC')) &
     (myOrders['Pair']!='BTCEUR') & (myOrders['Pair']!='BTCUSDT')].reset_index(drop=True)
 
+tableAlts = pd.DataFrame(float(0),index=myOrders['Pair'].unique(), columns=['Num Altcoin', 'Invested BTC', 'Current BTC', 'Gained BTC',
+            'Gained %', 'Avg Price', 'Last Purchasing Price', 'Last Selling Price', 'Current Price', 'Consolidated Gains']).astype(float)
+tableAlts['Gained %'] = tableAlts['Gained %'].astype(str)
+for _, row in myOrders.iterrows():
+    orderBTC = float(row['Trading total'][0:12].replace(',',''))
+    orderAlt = float(row['Executed'][0:12].replace(',',''))
+    if row['Side']=='BUY':
+        tableAlts.at[row['Pair'], 'Num Altcoin'] = tableAlts['Num Altcoin'][row['Pair']]  + orderAlt
+        tableAlts.at[row['Pair'], 'Invested BTC'] = tableAlts['Invested BTC'][row['Pair']] + orderBTC
+        tableAlts.at[row['Pair'], 'Avg Price'] = tableAlts['Invested BTC'][row['Pair']] / tableAlts['Num Altcoin'][row['Pair']]
+        tableAlts.at[row['Pair'], 'Last Purchasing Price'] = orderBTC / orderAlt                                           
+    elif row['Side']=='SELL':
+        ratioAlt = orderAlt / tableAlts['Num Altcoin'][row['Pair']]
+        tableAlts.at[row['Pair'], 'Consolidated Gains'] = tableAlts['Consolidated Gains'][row['Pair']] + orderBTC - ratioAlt*tableAlts['Invested BTC'][row['Pair']]
+        tableAlts.at[row['Pair'], 'Num Altcoin'] = tableAlts['Num Altcoin'][row['Pair']]  - orderAlt
+        tableAlts.at[row['Pair'], 'Invested BTC'] = tableAlts['Invested BTC'][row['Pair']] - ratioAlt*tableAlts['Invested BTC'][row['Pair']]
+        tableAlts.at[row['Pair'], 'Last Selling Price'] = orderBTC / orderAlt
+
+
+########################## Price update ###########################
 while True:
     binanceData = getPrices()
-    tableAlts = pd.DataFrame(float(0),index=myOrders['Pair'].unique(), columns=['Num Altcoin', 'Invested BTC', 'Current BTC', 'Gained BTC', \
-        'Gained %', 'Avg Price', 'Last Purchasing Price', 'Last Selling Price', 'Current Price', 'Consolidated Gains']).astype(float)
-    tableAlts['Gained %'] = tableAlts['Gained %'].astype(str)
+    for pair, row in tableAlts.iterrows():
+        if row["Invested BTC"]>0:
+            priceAlt = float(binanceData[binanceData['Pair']==pair].reset_index(drop=True)['CurrentPrice'])
+            tableAlts.at[pair, 'Current Price'] = priceAlt
+            tableAlts.at[pair, 'Current BTC'] = priceAlt * tableAlts['Num Altcoin'][pair]
+            tableAlts.at[pair, 'Gained BTC'] = tableAlts['Current BTC'][pair] - tableAlts['Invested BTC'][pair]
+            tableAlts.at[pair, 'Gained %'] = str(rounding(100*tableAlts['Gained BTC'][pair] / tableAlts['Invested BTC'][pair])) + ' %'
 
-    for _, row in myOrders.iterrows():
-        orderBTC = float(row['Trading total'][0:12].replace(',',''))
-        orderAlt = float(row['Executed'][0:12].replace(',',''))
-        priceAlt = float(binanceData[binanceData['Pair']==row['Pair']].reset_index(drop=True)['CurrentPrice'])
-        tableAlts.at[row['Pair'], 'Current Price'] = priceAlt
-
-
-        if row['Side']=='BUY':
-            tableAlts.at[row['Pair'], 'Num Altcoin'] = tableAlts['Num Altcoin'][row['Pair']]  + orderAlt
-            tableAlts.at[row['Pair'], 'Invested BTC'] = tableAlts['Invested BTC'][row['Pair']] + orderBTC
-
-            tableAlts.at[row['Pair'], 'Current BTC'] = priceAlt * tableAlts['Num Altcoin'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Gained BTC'] = tableAlts['Current BTC'][row['Pair']] - tableAlts['Invested BTC'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Gained %'] = str(rounding(100*tableAlts['Gained BTC'][row['Pair']] / tableAlts['Invested BTC'][row['Pair']])) + ' %'
-            tableAlts.at[row['Pair'], 'Avg Price'] = tableAlts['Invested BTC'][row['Pair']] / tableAlts['Num Altcoin'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Last Purchasing Price'] = orderBTC / orderAlt
-                                            
-            
-        elif row['Side']=='SELL':
-            ratioAlt = orderAlt / tableAlts['Num Altcoin'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Consolidated Gains'] = tableAlts['Consolidated Gains'][row['Pair']] + orderBTC - ratioAlt*tableAlts['Invested BTC'][row['Pair']]
-
-            tableAlts.at[row['Pair'], 'Num Altcoin'] = tableAlts['Num Altcoin'][row['Pair']]  - orderAlt
-            tableAlts.at[row['Pair'], 'Invested BTC'] = tableAlts['Invested BTC'][row['Pair']] - ratioAlt*tableAlts['Invested BTC'][row['Pair']]
-
-            tableAlts.at[row['Pair'], 'Current BTC'] = priceAlt * tableAlts['Num Altcoin'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Gained BTC'] = tableAlts['Current BTC'][row['Pair']] - tableAlts['Invested BTC'][row['Pair']]
-            tableAlts.at[row['Pair'], 'Last Selling Price'] = orderBTC / orderAlt
-           
-
-    tableAlts.dropna(inplace=True)
+    printedTable = tableAlts.dropna()
+    printedTable = printedTable.drop(printedTable[printedTable['Invested BTC']<10/float(binanceData[binanceData['Pair']=='BTCEUR']['CurrentPrice'])].index)
+    printedTable = printedTable.sort_values(by='Invested BTC', ascending=False)
+    printedTable = printedTable.filter(['Invested BTC', 'Current BTC', 'Avg Price' ,'Current Price', 'Gained BTC', 'Gained %'], axis=1)
 
     os.system('clear')
-    print("\n**************************************** CryptoBoard ****************************************")
+    print("\n**************************************** CryptoBoardPy ****************************************")
     print('BTC Price: %.0f EUR / %.0f USD' %(float(binanceData[binanceData['Pair']=='BTCEUR']['CurrentPrice']), \
         float(binanceData[binanceData['Pair']=='BTCUSDT']['CurrentPrice'])))
     print('Invested BTC: %.4f BTC - Current BTC: %.4f BTC - Current gains: %.4f BTC - Current gain: %.2f' \
@@ -82,12 +79,7 @@ while True:
         tableAlts['Gained BTC'].sum(),  \
         100*tableAlts['Gained BTC'].sum()/tableAlts['Invested BTC'].sum()) + ' %') 
     print('Consolidated BTC: %.4f BTC\n' %(tableAlts['Consolidated Gains'].sum()))
-
-    tableAlts = tableAlts.drop(tableAlts[tableAlts['Invested BTC']<10/float(binanceData[binanceData['Pair']=='BTCEUR']['CurrentPrice'])].index)
-    printedTable = tableAlts.filter(['Invested BTC', 'Current BTC', 'Avg Price' ,'Current Price', 'Gained BTC', 'Gained %'], axis=1)
-    printedTable = printedTable.sort_values(by='Invested BTC', ascending=False)
     print(printedTable)
-
     print('\nLast updated at {last_update}'.format(last_update = time.strftime("%m/%d/%Y %H:%M:%S",time.localtime())))
 
     time.sleep(60)
